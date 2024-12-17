c9d8ad6f000f6f220611dbb7c103e095cc4a5e1a
Ona0nline <onasihle123@gmail.com>
2024-12-17 11:48:54 +0200

    Design changed, squares progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,14 @@ module.exports = {
       "tropical indigo": "858AE3",
       "iris": "#613DC1",
       "indigo": "#4E148C",
-      "russian violet": "#2C0735"
+      "russian violet": "#2C0735",
+      "mint_cream": "#F0F7EE",
+      "cream": "#F3FFC6",
+      "dark_spring_green": "#306B34",
+      "mindaro":"#C3EB78",
+      "raspberry": "#DA1B5B",
+      "sage_brown":"#CDC6A5",
+      "light_yellow":"#FAF8D4",
     },
     extend: {
 
